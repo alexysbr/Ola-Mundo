1b9086e0d9600dc3e0aab02cd75d546e8d2d219b
Alex Y Suzuki <123485435+alexysbr@users.noreply.github.com>
2023-01-31 15:32:39 -0300

    ajustes

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function fjavascript(){
     conteudo.innerHTML=`<h3>JavaScript</h3>
     <ul>
         <li class="javs"><h4><a href="https://alexysbr.github.io/code/idade/idade.html">Verificador de idade</a></h4></li>
-        Site que recebe ano e gênero e retorna a idade.
+        <p>Site que recebe ano e gênero e retorna a idade.</p>
         <li class="javs"><h4><a href="https://alexysbr.github.io/code/periodo/periodo.html">Avalia o período do dia</a></h4></li>
-        Site que verifica o período do dia, informa a hora e o visual se adapta com o período do dia (Vai ser incluído uma opção para alterar manualmente a hora, para ficar mais fácil de verificar os efeitos visuais e também uma atualização automática do site).
+        <p>Site que verifica o período do dia, informa a hora e o visual se adapta com o período do dia (Vai ser incluído uma opção para alterar manualmente a hora, para ficar mais fácil de verificar os efeitos visuais e também uma atualização automática do site).</p>
         <li class="javs"><h4><a href="https://alexysbr.github.io/code/tictactoy/tic.html">Jogo da Velha</a></h4></li>
-        Tradicional jogo da velha (Pretendo colocar uma IA que jogará aleatoriamente e outra que não perde).`
+        <p>Tradicional jogo da velha (Pretendo colocar uma IA que jogará aleatoriamente e outra que não perde).</p>`
 }
