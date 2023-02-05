293ceb76127522fa1a9852ff9bf78beedb15a10d
Alex Y Suzuki <123485435+alexysbr@users.noreply.github.com>
2023-02-05 19:00:07 -0300

    ajustes no site

--- script.js
+++ script.js
@@ -10,5 +10,27 @@ function fjavascript(){
         <li class="javs"><h4><a href="https://alexysbr.github.io/code/periodo/periodo.html">Avalia o período do dia</a></h4></li>
         Site que verifica o período do dia, informa a hora e o visual se adapta com o período do dia (Vai ser incluído uma opção para alterar manualmente a hora, para ficar mais fácil de verificar os efeitos visuais e também uma atualização automática do site).
         <li class="javs"><h4><a href="https://alexysbr.github.io/code/tictactoy/tic.html">Jogo da Velha</a></h4></li>
-        Tradicional jogo da velha (Pretendo colocar uma IA que jogará aleatoriamente e outra que não perde).`
+        Tradicional jogo da velha (Pretendo colocar uma IA que jogará aleatoriamente e outra que não perde).
+    </ul>`
+}
+function fpython(){
+    conteudo.innerHTML=`<h3>Python</h3>
+    <a href="https://github.com/alexysbr/Desafios-Python">Link</a> do repositório de códigos em Python dos desafios do Curso em Vídeo, futuramente pretendo colocar de forma visual neste site.`
+}
+function fjogos(){
+    conteudo.innerHTML=`<h3>Jogos</h3>
+    <ul>
+    <li><a href="https://alexysbr.github.io/code/tictactoy/tic.html">Jogo da Velha</a></h4></li>
+    Tradicional jogo da velha em JavaScript (Pretendo colocar uma IA que jogará aleatoriamente e outra que não perde).
+    </ul>`
+}
+function fcertificados(){
+    conteudo.innerHTML=`<h3>Certificados</h3>
+    Certificados adquiridos na conclusão das aulas do Curso em Video que podem ser validados no <a href="https://www.cursoemvideo.com/validacao-de-certificado/">site</a>
+    <ul>
+        <li>Github: 9F228-A18C-2</li>
+        <li>JavaScript: 9F228-6C61-4</li>
+        <li>HTML e CSS Modulo 1: </li>
+        <li>Python Modulo 1: 9F228-66E2-9</li>
+    </ul>`
 }
